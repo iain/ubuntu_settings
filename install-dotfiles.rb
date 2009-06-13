d524a7275a8aac1cb3562b06124e53fdc5d55d38
Iain Hecker <github@iain.nl>
2009-06-13 21:52:46 +0200

    make install dotfile better

--- install-dotfiles.rb
+++ install-dotfiles.rb
@@ -1,5 +1,6 @@
 require 'fileutils'
 files = Dir["dotfiles/*"].each do |f|
+  `mv ~/.#{File.basename(f)} ~/#{File.basename(f)}.bak`
   cmd = "ln -fs #{File.expand_path(f)} ~/.#{File.basename(f)}"
   puts cmd
   `#{cmd}`
